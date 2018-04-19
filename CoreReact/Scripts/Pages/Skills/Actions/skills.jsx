db939d9034a67bca4455aa2c84c2d0a7f05ab41b
Aleksandar Trajkov <aleksandar.trajkov@allocatesoftware.com>
2018-04-19 16:15:26 +0200

    removed canceling request until further implementation is found

--- CoreReact/Scripts/Pages/Skills/Actions/skills.jsx
+++ CoreReact/Scripts/Pages/Skills/Actions/skills.jsx
@@ -1,17 +1,12 @@
 ﻿import { actionTypes } from './../constants'
 import Ajax from './../../../Infrastructure/Ajax'
 
-let currentSkillsRequest;
 function getSkills() {
     return dispatch => {
-        if (currentSkillsRequest && currentSkillsRequest.abort) {
-            currentSkillsRequest.abort();
-        }
-        currentSkillsRequest = Ajax.get('/api/skills');
-        currentSkillsRequest.then(function (response) {
-            dispatch(getSkillsSuccess(response.data));
-        });
-        return currentSkillsRequest;
+        return Ajax.get('/api/skills')
+            .then(function (response) {
+                dispatch(getSkillsSuccess(response.data));
+            });
     };
 }
 function getSkillsSuccess(data) {
