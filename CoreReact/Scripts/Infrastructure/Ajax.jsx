239b2eb56fcaea312b4dc03a1e0da7d1d0f083c0
Ace Trajkov <aleksandar.trajkov@outlook.com>
2018-04-22 22:31:12 +0200

    added catch to ajax requests

--- CoreReact/Scripts/Infrastructure/Ajax.jsx
+++ CoreReact/Scripts/Infrastructure/Ajax.jsx
@@ -1,17 +1,24 @@
 ﻿import axios from 'axios';
 
 class Ajax {
+    static executeRequest(method, url, data) {
+        return axios[method](url, data)
+            .catch(function (error) {
+                console.log(error);
+            })
+    }
+
     static get(url) {
-        return axios.get(url);
+        return Ajax.executeRequest('get', url);
     }
     static post(url, data) {
-        return axios.post(url, data);
+        return Ajax.executeRequest('post', url, data);
     }
     static put(url, data) {
-        return axios.put(url, data);
+        return Ajax.executeRequest('put', url, data);
     }
     static delete(url) {
-        return axios.delete(url);
+        return Ajax.executeRequest('delete', url);
     }
 }
 
