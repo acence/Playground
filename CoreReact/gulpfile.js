08096c71a1336ec06851bc277c38e18c5266bfb3
Ace Trajkov <aleksandar.trajkov@outlook.com>
2018-05-06 20:00:19 +0200

    added eslint

--- CoreReact/gulpfile.js
+++ CoreReact/gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
+var eslint = require('gulp-eslint');
 var browserify = require('browserify');
 var babelify = require('babelify');
 var source = require('vinyl-source-stream');
@@ -73,6 +74,24 @@ gulp.task('build:css-sass', () => {
         .pipe(sass().on('error', sass.logError))
         .pipe(rename(fixNameCSS))
         .pipe(gulp.dest('./wwwroot/css'));
+});
+
+
+gulp.task('lint', () => {
+    // ESLint ignores files with "node_modules" paths.
+    // So, it's best to have gulp ignore the directory as well.
+    // Also, Be sure to return the stream from the task;
+    // Otherwise, the task may end before the stream has finished.
+    return gulp.src(['Scripts/*.jsx'])
+        // eslint() attaches the lint output to the "eslint" property
+        // of the file object so it can be used by other modules.
+        .pipe(eslint())
+        // eslint.format() outputs the lint results to the console.
+        // Alternatively use eslint.formatEach() (see Docs).
+        .pipe(eslint.format())
+        // To have the process exit with an error code (1) on
+        // lint error, return the stream and pipe to failAfterError last.
+        .pipe(eslint.failAfterError());
 });
 
 gulp.task('watch', function () {
